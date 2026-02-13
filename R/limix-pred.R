062269361110aac773ba8aa5600e72a433fb5a3d
frank hull <frankiethull@gmail.com>
2026-02-12 23:31:21 -0700

    reg preds are now arrays instead of tensors

--- R/limix-pred.R
+++ R/limix-pred.R
@@ -2,14 +2,19 @@
 #'
 #' @param object an object returned from limix_fit
 #' @param X_test a set of predictors
+#' @param task_type either `Classification` or `Regression`
 #' @param ... pass through for prediction method
 #'
 #' @returns predictions
 #'
 #' @export
-limix_predict <- \(object, X_test, ...) {
+limix_predict <- \(object, X_test, task_type = "Classification", ...) {
   model <- object$model
 
-  predictions <- model$predict(object$X_train, object$y_train, X_test, ...)
+  predictions <- model$predict(object$X_train, object$y_train, X_test, task_type = task_type, ...)
+  if(task_type == "Regression"){
+    predictions <- predictions$to('cpu')$numpy()
+  }
   predictions
 }
+
